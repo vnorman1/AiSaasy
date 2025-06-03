b86ef811867f725f1c99aba39c3a8f1f415c1ade
vnorman1 <vajdanorman4@gmail.com>
2025-06-03 18:17:02 +0200

    Bump brain version to 1.1.0 for updated configuration

--- src/script.js
+++ src/script.js
@@ -129,7 +129,7 @@ class ModularAIAssistant {
         }
     }createDefaultBrain() {
         this.brain = {
-            version: "1.0.0",
+            version: "1.1.0",
             config: {
                 apiKey: "",
                 behavior: {
